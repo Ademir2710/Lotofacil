82e00b848bd7056a2b272a7faafd26bc1669f4ab
Ademir2710 <ademir.i.goncalves@gmail.com>
2021-03-12 22:41:16 -0300

    Adicionado novo arquivo

---
+++ Loto Facil.js
@@ -0,0 +1,57 @@
+var ademir = "o melhor"
+
+// a = Math.round(Math.random()*25)
+// b = Math.round(Math.random()*25)
+// c = Math.round(Math.random()*25)
+// d = Math.round(Math.random()*25)
+// e = Math.round(Math.random()*25)
+// f = Math.round(Math.random()*25)
+// g = Math.round(Math.random()*25)
+// h = Math.round(Math.random()*25)
+// i = Math.round(Math.random()*25)
+// j = Math.round(Math.random()*25)
+// k = Math.round(Math.random()*25)
+// l = Math.round(Math.random()*25)
+// m = Math.round(Math.random()*25)
+// n = Math.round(Math.random()*25)
+// o = Math.round(Math.random()*25)
+// p = Math.round(Math.random()*25)
+// q = Math.round(Math.random()*25)
+// r = Math.round(Math.random()*25)
+
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+// console.log(e)
+// console.log(f)
+// console.log(g)
+// console.log(h)
+// console.log(i)
+// console.log(j)
+// console.log(k)
+// console.log(l)
+// console.log(m)
+// console.log(n)
+// console.log(o)
+// console.log(p)
+// console.log(q)
+// console.log(r)
+
+
+
+let contador = 1
+// while
+while (contador < 15+1) {
+    console.log(Math.round(Math.random()*25))
+    contador++ //necessario para fazer o loop
+    
+}
+
+
+
+
+
+
+
---
+++ loto.js
@@ -0,0 +1,7 @@
+let contador = 1
+// while
+while (contador < 15+1) {
+    console.log(Math.round(Math.random()*25))
+    contador++ //necessario para fazer o loop
+    
+}
